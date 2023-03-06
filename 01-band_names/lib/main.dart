cd2bdbc314f5faa277c8cdcf71981eb3c36a4802
Nicolas <na796840@gmail.com>
2023-03-06 01:49:22 -0500

    backend

--- 01-band_names/lib/main.dart
+++ 01-band_names/lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:band_names/services/socket_service.dart';
 import 'package:flutter/material.dart';
-
+import 'package:provider/provider.dart';
 import 'pages/home.dart';
+import 'pages/status.dart';
 
 void main() => runApp(const MyApp());
 
@@ -9,11 +11,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Material App',
-      initialRoute: "home",
-      routes: {"home": (_) => HomePage()},
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_) => SocketService())],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Material App',
+        initialRoute: "home",
+        routes: {"home": (_) => HomePage(), "status": (_) => StatusPage()},
+      ),
     );
   }
 }
--- 01-band_names_server/sockets/socket.js
+++ 01-band_names_server/sockets/socket.js
@@ -1,11 +1,28 @@
 
 const { io } = require("../index.js");
+const Bands = require("../models/bands.js");
+const Band = require("../models/band.js");
 
+const bands  = new Bands();
+
+bands.addBand(new Band("metallica"));
+bands.addBand(new Band("Queen"));
+bands.addBand(new Band("Linkin Park"));
+bands.addBand(new Band("Bon Jovi"));
+
+
+console.log(bands);
 
 //! Mensaje de Sockets
 
 io.on('connection', client => {
     console.log("Cliente conectado");
+
+      client.emit("active-bands", bands.getBands());
+
+
+
+
     client.on('disconnect', () => { 
         console.log("Cliente desconectado")
      });                                                       //! "IO" es todo el servidor
@@ -14,9 +31,15 @@ io.on('connection', client => {
         console.log("Mensaje!!", payload);
 
         io.emit("mensaje", {admin: "Nuevo mensaje"});
+     });
 
 
-     })
+     client.on("emitir-mensaje", (payload) => {
+      //console.log(payload);
+     // io.emit("nuevo-mensaje", payload); //emite a todos
+     client.broadcast.emit("nuevo-mensaje", payload);  //! emite a todos menos el que lo emitio
+     });
+
 
 
   });
